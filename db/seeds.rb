6b011ce5b3cfa44c7905e081be7f7b9b113c72a6
Josh Calkins <jcalkins89@gmail.com>
2016-02-05 19:12:04 -0500

    Checkpoint 39 Posts and Users: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+# Create Users
+5.times do
+  User.create!(
+
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -11,6 +22,7 @@ topics = Topic.all
 50.times do
 # #1
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -28,7 +40,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'jcalkins89@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
