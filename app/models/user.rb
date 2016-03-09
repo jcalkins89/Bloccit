1746229fee6c2ff519a78c68c8e54f6eda9d63f6
Josh Calkins <jcalkins89@gmail.com>
2016-03-08 21:13:52 -0500

    Checkpoint 48 Building an API: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,24 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save { self.role ||= :member }
 
-# #3
+  before_create :generate_auth_token
+
+
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-# #4
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-# #5
+
  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
  validates :password, length: { minimum: 6 }, allow_blank: true
-# #6
+
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 100 },
             format: { with: EMAIL_REGEX }
 
-# #7
+
   has_secure_password
 
   enum role: [:member, :admin]
@@ -33,5 +35,12 @@ class User < ActiveRecord::Base
   def avatar_url(size)
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
+  def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64)
+      break unless User.find_by(auth_token: auth_token)
+    end
   end
 end
